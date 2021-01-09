198f5a79f984f0673e3ec64dbd73a7e603fd6d7e
tiagoveri <tiagoverisjc@gmail.com>
2021-01-09 18:26:48 -0300

    Finish front-web

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css';
-import Navbar from './Navbar';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Routes from './Routes';
 
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
         <Routes />
+        <ToastContainer/>
     </>
   );
 }

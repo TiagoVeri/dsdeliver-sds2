f77f55a324149a4a44e641fc150abd935f3caa8e
tiagoveri <tiagoverisjc@gmail.com>
2021-01-17 17:50:35 -0300

    Add Orders list

--- front-mobile/App.tsx
+++ front-mobile/App.tsx
@@ -1,10 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Header from './scr/Header';
 import { useFonts, OpenSans_400Regular, OpenSans_700Bold } from '@expo-google-fonts/open-sans';
 import AppLoading from 'expo-app-loading';
-import Home from './scr/Home';
+import Routes from './scr/Routes';
 
 export default function App() {
  
@@ -21,8 +20,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <Header/>
-      <Home />
+      <Routes />
     </View>
   );
 }
